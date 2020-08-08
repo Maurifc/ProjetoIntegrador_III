b0bd7bc072a3ab191a72da3474ada1c22e8cb9ff
Lucas <lucastsantos.developer@gmail.com>
2020-08-07 21:22:49 -0300

    doc: adiciona comentario na funcao que define a funcao matematica que sera executada

--- main.py
+++ main.py
@@ -6,32 +6,32 @@ from Populacao import Populacao
 TAMANHO_INDIVIDUO = 8
 TAMANHO_POPULACAO = 100
 
+# Função resposável por definir a função matemática que sera usada no algoritmo
 def f(x):
-  return 5*x**2 + 10*x - 10                     # 5x² + 10x - 10                
+  return 5*x**2 + 50*x - 20                     # 5x² + 50x - 20                
   # return x**3 - 4*x**3 + 4*x**2 + 10            # x³ - 4x³ + 4x² + 10
   # return math.cos(3*x) + 3                      # cos(3*x) + 3
-  # return 8*x**2 + 2*math.cos(5*x) + 12*x + 12     # 8x² + 2 cos(5x) + 12x + 12
 
-# inicializacao do grafico
 plt.title('Gráfico do Fitness das gerações')
 plt.xlabel('Geração')
 plt.ylabel('Fitness')
                                                                                
 # inicializacao da populacao parametros: (TAMANHO_POPULACAO, TAMANHO_INDIVIDUO, POSSUI_MINIMO, FUNCAO_MATEMATICA)
-populacao = Populacao(TAMANHO_POPULACAO, TAMANHO_INDIVIDUO, True, f)
+populacao = Populacao(TAMANHO_POPULACAO, TAMANHO_INDIVIDUO, False, f)
 populacao.inicializar()
 populacao.arrMelhoresIndividuoes.append(populacao.melhorIndividuo.fitness)
 
-while(populacao.contadorGeracao < 20):
+while(populacao.contadorGeracao < 300):
 
   populacao.calcularFitness()
 
-  populacao.selecao(0.7) # a porcentagem se refere a quantidade de individuos que sobreviverao à selecao
+  populacao.selecao(0.7) # o percentual se refere a quantidade de individuos que sobreviverao à selecao
 
-  populacao.reproducao(0.46) # a porcentagem se refere a quantidade de individuos que farao a reproducao
+  populacao.reproducao(0.46) # o percentual se refere a quantidade de novos individuos gerados
 
   populacao.arrMelhoresIndividuoes.append(populacao.melhorIndividuo.fitness)
-  populacao.mutacao(0.02)
+
+  populacao.mutacao(0.02) # o percentual se refera a chance da mutação acontecer
 
   populacao.contadorGeracao += 1
   populacao.geracoes.append(populacao.contadorGeracao)
